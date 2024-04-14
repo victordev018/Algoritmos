74c611fbd3db9dbffd6481eef9c74200b02803d8
João Victor <joawrt8520@gmail.com>
2024-04-14 17:06:20 -0300

    questão 14, fabio03 - FOR

---
+++ Lista fabio (soluções)/ESTRUTURA REPETIÇÃO FOR/Atividade Q/questao14.js
@@ -0,0 +1,8 @@
+// Leia N, calcule e escreva o maior quadrado menor ou igual a N. Por exemplo, se N for igual a 38, o
+// maior quadrado menor que 38 é 36 (quadrado de 6).
+
+function main(){
+
+}
+
+main()

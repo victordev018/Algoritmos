ad4c5925b7cf8ca3b8d2b07f2876e0b4f8f16e11
João Victor <joawrt8520@gmail.com>
2024-04-15 09:10:22 -0300

    mudando nome da veriável

--- Lista fabio (soluções)/ESTRUTURA REPETIÇÃO FOR/Atividade Q/questao14.js
+++ Lista fabio (soluções)/ESTRUTURA REPETIÇÃO FOR/Atividade Q/questao14.js
@@ -5,15 +5,14 @@ import { get_number } from "./utils/io_utils.js"
 
 function main(){
     let N = get_number("\n> N value: ")
-    let quad = 0
+    let quadPerfect = 0
 
-    for (let c = N; quad == 0; c-- ){
+    for (let c = N; quadPerfect == 0; c-- ){
         if ( (c**0.5) % 1 == 0 ){ // se o resto da divisão da raiz quadrada de C por 1 for igual a 0,
-            quad = c              // então C é um quadrado perfeito.
+            quadPerfect = c              // então C é um quadrado perfeito.
         }
     }
-    console.log(`O quadrado mais proximo: ${quad}\n`)
-
+    console.log(`O quadrado mais proximo: ${quadPerfect}\n`)
 }
 
 main()

a0823c50001e86ae342310c6c9c4766bf7e0aeae
João VIctor <joawrt8520@gmail.com>
2024-07-02 20:27:00 -0300

    feat: resolvida a questão 25 lista 03 - while

---
+++ Lista fabio (soluções)/ESTRUTURA REPETIÇÃO 04 WHILE/Atividade M/questao25.js
@@ -0,0 +1,77 @@
+// Em uma eleição presidencial existem 3 (três) candidatos. Os votos são informados através de códigos.
+// Os dados utilizados para a contagem dos votos obedecem à seguinte codificação:
+// · 1, 2, 3 = voto para os respectivos candidatos;
+// · 9 = voto nulo;
+// · 0 = voto em branco;
+// Escreva um algoritmo que leia o código votado por N eleitores. Ao final, calcule e escreva:
+// a) total de votos para cada candidato;
+// b) total de votos nulos;
+// c) total de votos em branco;
+// d) quem venceu a eleição.
+
+import { get_number } from "./utils/io_utils.js"
+
+function main(){
+    console.log('\n------- ELEICAO 2024 --------')
+    console.log("> 1 - Lula \n> 2 - Bolsonaro \n> 3 - Ciro Gomes \n> 9 - Nulo \n> 0 - Branco")
+    console.log("-------------------------------")
+
+    let totVotosLula = 0
+    let totVotosBolso = 0
+    let totVotosCiro = 0
+    let totVotosNulo = 0
+    let totVotoBranco = 0
+    let quemVenceu = ""
+
+    let contador = 1
+
+    let nEleitores = get_number("> quantidade de eletores: ")
+    while (contador <= nEleitores){
+        console.log(`\n> ${contador}° eleitor:`)
+        let voto = get_number("voto: ")
+        if (voto == 1){
+            totVotosLula++
+        }
+        else if (voto == 2){
+            totVotosBolso++
+        }
+        else if(voto == 3){
+            totVotosCiro++
+        }
+        else if(voto == 9){
+            totVotosNulo++
+        }
+        else if(voto == 0){
+            totVotoBranco++
+        }
+
+        contador++
+    }
+
+    quemVenceu = apuradorDeVotos(totVotosLula, totVotosBolso, totVotosCiro)
+
+    const resultado = `
+    |---- RESULTADO ELEIÇÂO ----|
+    |> Lula: ${totVotosLula} votos
+    |> Bolso: ${totVotosBolso} votos
+    |> Ciro: ${totVotosCiro} votos
+    |> Votos Nulos: ${totVotosNulo}
+    |> Votos Brancos: ${totVotoBranco}
+    |> 
+    |> Vencedor: ${quemVenceu} \n`
+
+    console.log(resultado)
+}   
+// função para calcular quem foi o vencedor da eleição, vence quem tiver mais votos.
+function apuradorDeVotos(lula, bolso, ciro){
+    if (lula> bolso && lula > ciro){
+        return "Lula"
+    }
+    else if (bolso > lula && bolso > ciro){
+        return "Bolsonaro"
+    }
+    else{
+        return "Ciro Gomes"
+    }
+}
+main()

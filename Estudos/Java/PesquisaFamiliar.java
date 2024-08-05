ddc7ac201b1d366a34df1255cc85d3f6af3c5c5c
João VIctor <joawrt8520@gmail.com>
2024-08-05 14:53:21 -0300

    feat: questão feita em sala usando for

---
+++ Estudos/Java/PesquisaFamiliar.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PesquisaFamiliar{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("> quantidade de familias: ");
+        Integer numFamilias = in.nextInt();
+
+        Double mediaSalarioFamilias = 0.0;
+        Double porcentagemFamilias = 0.0;
+        Double mediaFilhosFamilias = 0.0;
+
+        for (int i = 0; i < numFamilias; i++){
+            System.out.print("\n> Renda familiar: ");
+            Double rendaFamilia = in.nextDouble();
+            System.out.print("> Número de filhos: ");
+            Integer numFilhos = in.nextInt();
+
+            // computando dados
+            mediaSalarioFamilias += rendaFamilia;
+            mediaFilhosFamilias += numFilhos;
+
+            if (rendaFamilia > 1000.0){
+                porcentagemFamilias++;
+            }
+        }
+
+        mediaSalarioFamilias /= numFamilias;
+        mediaFilhosFamilias /= numFamilias;
+        porcentagemFamilias /= numFamilias * 100.0;
+
+        // saida de dados
+        System.out.println("> media salários: R$ " + mediaSalarioFamilias);
+        System.out.println("> media filhos: " + mediaFilhosFamilias);
+        System.out.println("> Percentual de familias renda: " + porcentagemFamilias + "%");
+    }
+}
